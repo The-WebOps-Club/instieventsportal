af283fd2245ab9f442bb67cba60384ee99b2ca23
venketepprasad <venkichandran1996@gmail.com>
2015-08-21 18:09:22 +0530

    scoreboard,some authlocal corrections

--- server/api/scoreboard/index.js
+++ server/api/scoreboard/index.js
@@ -2,14 +2,15 @@
 
 var express = require('express');
 var controller = require('./scoreboard.controller');
-
+var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', controller.index);
 // router.get('/:id', controller.show);
-router.post('/', controller.create);
+// router.post('/',auth.hasAdminRole('sec'), controller.create);
 router.get('/initialsetup',auth.hasAdminRole('sec'),controller.setup);
-// router.put('/:id', controller.update);
+// router.get('/initialsetupFinal',auth.hasAdminRole('sec'),controller.setupFinal);
+router.put('/',auth.hasAdminRole('sec'), controller.update);
 // router.patch('/:id', controller.update);
 // router.delete('/:id', controller.destroy);
 
--- server/api/scoreboard/scoreboard.model.js
+++ server/api/scoreboard/scoreboard.model.js
@@ -3,19 +3,26 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var HostelSchema = new Schema({
-  name: {type: String,required : true, unique: true}
-});
 
+
+var ScorecardSchema = new Schema({
+	hostelId : { type:Schema.Types.ObjectId, ref:'HostelSchema' , unique: true},
+  	score : { type: Number,required:true,default:0}
+  });
+
+var HostelSchema = new Schema({
+  name: {type: String,required : true, unique:true }
+});
 
 var ScoreboardSchema = new Schema({
-  category: String,
-  scorecard:
-  [
-  	hostelId : { type:Schema.Types.ObjectId, ref:'HostelSchema', unique : true },
-  	score : { type: String,required:true,default:0}
-  ]
+  category: {type:String , required: true},
+  scorecard: [ScorecardSchema]
 });
 
-module.exports = mongoose.model('Hostel', HostelSchema);
-module.exports = mongoose.model('Scoreboard', ScoreboardSchema);
+var Scoreboard = mongoose.model('Scoreboard', ScoreboardSchema);
+var Hostel= mongoose.model('Hostel', HostelSchema);
+var Scorecard = mongoose.model('Scorecard', ScorecardSchema);
+
+exports.Scoreboard = Scoreboard;
+exports.Hostel = Hostel;
+exports.Scorecard=Scorecard;
