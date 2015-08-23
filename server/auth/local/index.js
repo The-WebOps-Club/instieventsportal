2cef17747d372e431efa3eb948ba82ab94b4065e
venketepprasad <venkichandran1996@gmail.com>
2015-08-23 16:26:40 +0530

    club page opens on authlocal

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -7,10 +7,47 @@ var User=require('../../api/user/user.model');
 var jwt = require('jsonwebtoken');
 var config = require('../../config/environment');
 var router = express.Router();
+var ClubSchema = require('../../api/club/club.model');
 
 var validationError = function(res, err) {
   return res.json(422, err);
 };
+
+router.post('/mobile', function(req, res, next) {
+  passport.authenticate('user-local', function (err, user, info) {
+    var error = err || info;
+     if (error) {
+      if (error.error == 404){
+        user = new User(req.body);
+       user.provider = 'local';
+       user.role = 'user';
+       user.save(function(err, user) {
+        if (err) return validationError(res, err);
+      });
+      }
+      else
+      return res.json(401, error);
+    }
+    
+    var token = auth.signToken(user._id, user.role);
+    var i,j;
+  ClubSchema.Club.find(function (err, clubs) {
+    ClubSchema.Subscribe.find(function (err, subscribe){
+      for(i=0;i<clubs.length;i++)
+      {
+        for(j=0;j<subscribe.length;j++)
+        {
+          
+          if(subscribe[j].club+'1'==clubs[i]._id+'1' && subscribe[j].user +'1'== user._id+'1')
+           { clubs[i].isSubscribed=true; 
+              console.log(clubs[i].isSubscribed);}
+        }
+      }
+       res.json(200,{ token: token, user:user ,clubs : clubs});
+    });    
+  });
+  })(req, res, next)
+});
 
 router.post('/', function(req, res, next) {
   passport.authenticate('user-local', function (err, user, info) {
@@ -27,22 +64,11 @@ router.post('/', function(req, res, next) {
       else
       return res.json(401, error);
     }
-
-    // if (!user) 
-    // { 
-    //    user = new User(req.body);
-    //    user.provider = 'local';
-    //    user.role = 'user';
-    //    user.save(function(err, user) {
-    //     if (err) return validationError(res, err);
-    //   });
-    // }
-    // if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
-
-    var token = auth.signToken(user._id, user.role);
-    res.json({token: token,user: user});
+     var token = auth.signToken(user._id, user.role);
+  res.json(200,{ token: token, user:user});
   })(req, res, next)
 });
+
 
 router.post('/admin/', function(req, res, next) {
   passport.authenticate('admin-local', function (err, user, info) {
