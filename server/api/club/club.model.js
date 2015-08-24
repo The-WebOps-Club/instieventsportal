d91e0dd377dbea313b264d831fb8787f48778cb4
Aqel Ahammad <aqel123@gmail.com>
2015-08-24 20:10:22 +0530

    resolved conflicts

--- server/api/club/club.model.js
+++ server/api/club/club.model.js
@@ -5,11 +5,7 @@ var mongoose = require('mongoose'),
 var ConvenorSchema = new Schema({
   name: { type : String, required : true },
   email: { type : String, required: true },
-<<<<<<< HEAD
   phoneNumber: { type : String, required : true }
-=======
-  phoneNumber: { type : String, required : true },
->>>>>>> b1aa7ef291280bc9e6b01cd556b843f7c39efc78
 });
 
 var SubscriptionSchema = new Schema({
